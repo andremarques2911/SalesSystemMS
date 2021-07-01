381d078fe05370a253b05441d4caf8022a81d667
André <Marques>
2021-07-01 15:17:34 -0300

    Ajuste

--- backend/StockMS/src/main/java/com/projearq/stockMS/adapters/configurations/rollbackstockqueue/RollbackStockReceiver.java
+++ backend/StockMS/src/main/java/com/projearq/stockMS/adapters/configurations/rollbackstockqueue/RollbackStockReceiver.java
@@ -1,16 +1,13 @@
-//package com.projearq.stockMS.adapters.configurations.rollbackstockqueue;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Component;
-//
-//@Slf4j
-//@Component
-//public class RollbackStockReceiver {
-////    @Autowired
-////    private ServicoCotacao servicoCotacao;
-//
-//    public void rollbackStock(String message) {
-//        String dados[] = message.split(",");
-//        log.info("Received <" + message + ">");
-////        servicoCotacao.novaCotacao(dados[0], dados[1],Double.parseDouble(dados[2]));
-//    }
-//}
+package com.projearq.stockMS.adapters.configurations.rollbackstockqueue;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class RollbackStockReceiver {
+
+    public void receive(String message) {
+        log.info("Received > " + message);
+    }
+
+}
